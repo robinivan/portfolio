e1c497a988dedf9e093a962de236136b419e302d
robinivan <ivan.robin@epitech.digital>
2022-02-22 17:14:56 +0300

    1234

--- portfolio_of_ivan_robin/src/components/About.js
+++ portfolio_of_ivan_robin/src/components/About.js
@@ -7,6 +7,16 @@ export default function About() {
     return(
         <div className='background'>
             <Nav_bar page="1"/>
+            <div className="body_box">
+                <div className="column1">
+                    <h1 className="title">
+                       About
+                    </h1>
+                </div>
+                <div className='column2'>
+
+                </div>
+            </div>
             <Footer/>
         </div>
     )
